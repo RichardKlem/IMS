d4d25ba4473245ea16444df1e57ef969a588cb81
Richard <richard.klem@seznam.cz>
2020-12-02 18:14:03 +0100

    Create matrix class for further usage

---
+++ Matrix.h
@@ -0,0 +1,33 @@
+//
+// Created by Richa on 02-Dec-20.
+//
+
+#ifndef IMS_MATRIX_H
+#define IMS_MATRIX_H
+
+#include <vector>
+#include "Cell.h"
+
+using namespace std;
+class Matrix {
+    vector<vector<Cell> >m;
+public:
+    Matrix(unsigned int x, unsigned int y) {
+        m.resize(x, vector<Cell>(y, Cell()));
+    }
+    class MatrixRow {
+    private:
+        vector<Cell>& row;
+    public:
+        explicit MatrixRow(vector<Cell>& r) : row(r) {
+        }
+        Cell& operator[](unsigned int y) {
+            return row.at(y);
+        }
+    };
+    MatrixRow operator[](unsigned int x) {
+        return MatrixRow(m.at(x));
+    }
+};
+
+#endif //IMS_MATRIX_H
