50e34a13dfe9801eea23f5a19ef9402e3346a2cb
Richard Klem <rklem@codasip.com>
2020-12-02 22:34:02 +0100

    Add argument parsing.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,57 @@
 #include <iostream>
+#include <getopt.h>
 #include "CellularAutomaton.h"
+#include "my_string.h"
+#include "main.h"
 
-int main() {
-    CellularAutomaton CA(100, 100);
+/**
+ * @brief Funkce zpracuje argumenty a nastaví podle nich proměnné.
+ * @param argc počet argumentů
+ * @param argv ukazatel na pole argumentů
+ * @param initInfectionRate ukazatel na proměnnou
+ */
+void argParse(int argc, char **argv, unsigned int * initInfectionRate) {
+    int c, option_index;
+    while ((c = getopt_long(argc, argv, short_options, long_options, &option_index)) != -1)
+    {
+        str2int_struct_t tmp = str2int(optarg);
+        str2int_struct_t *p_tmp = &tmp;
+        switch (c)
+        {
+            case 'h':
+                fprintf(outFile,"%s", help_text);
+                exit (OK);
+            case 'n':
+                if (p_tmp->status) {
+                    if (p_tmp->num < 0) {
+                        cout << p_tmp->num << endl;
+                        fprintf(errFile, "\n%s   Nesprávná hodnota čísla. Hodnota musí být ostře větší než 0.%s\n\n", RED, RST);
+                        exit(BAD_ARG_VALUE);
+                    }
+                    *initInfectionRate = p_tmp->num;
+                }
+                else {
+                    cout << p_tmp->num << endl;
+                    fprintf(errFile, "\n%s   Nesprávná hodnota čísla. Hodnota musí být ostře větší než 0.%s\n\n", RED, RST);
+                    exit(BAD_ARG_VALUE);
+                }
+                break;
+            default:
+                exit(UNKNOWN_PARAMETER);
+        }
+    }
+}
 
+int main(int argc, char *argv[]) {
+    unsigned int initInfectionRate = 0;
+
+    if (argc > 1)
+        argParse(argc, argv, &initInfectionRate);
+
+    CellularAutomaton CA(1420, 3550);
     CA.getMatrix()[10][10].setState(1);
-
-    std::cout << "Hello, World! " << std::endl << CA.getMatrix()[10][10].getState() << std::endl << CA.getMatrix()[10][11].getState();
+    cout << CA.getMatrix().size / initInfectionRate << endl;
     return 0;
 }
+
+
--- main.h
+++ main.h
@@ -5,4 +5,45 @@
 #ifndef IMS_MAIN_H
 #define IMS_MAIN_H
 
+#define YELLOW "\033[0;93m"
+#define RED "\033[0;91m"
+#define RST "\033[0m"
+
+
+FILE * outFile = stdout;
+FILE * errFile = stdout;
+// Definice dlouhých přepínačů.
+static struct option long_options[] =
+        {
+                {"help",   no_argument,        nullptr, 'h'},
+                {"num",    optional_argument,  nullptr, 'n'},
+                {nullptr, 0, nullptr, 0}
+        };
+// Definice krátkých přepínačů.
+char *short_options = (char*)"hn:";
+
+enum EXIT_CODES {
+    OK = 0,
+    FAILURE = 1,
+    BAD_ARG_VALUE = 11,
+    UNKNOWN_PARAMETER = 12};
+
+const char * help_text = "\n***Nápověda k CA simuláru***\n"
+                         "Překlad:\n"
+                         "  - maken\n\n"
+                         "Spuštění:\n"
+                         "  ./ebola [-i <nazev_rozhrani>| -r <nazev_souboru.pcapng>] [volitelné argumenty]\n"
+                         "  Možné parametry POVINNÉ:\n"
+                         "    -i <nazev_rozhrani> (Rozhraní, na kterém se bude poslouchat).\n"
+                         "  nebo\n"
+                         "    -r <nazev_souboru> (Soubor se zachycenými daty).\n"
+                         "            Nebude-li ani jeden z těchto parametrů uveden, "
+                         "            vypíše zkrácená nápověda se seznam aktivních rozhraní)\n"
+                         "  Možné parametry VOLITELNÉ:\n"
+                         "    -n | --num int: Počáteční počet nakažených.\n"
+
+                         "  Krátké parametry je možné zadávat ve tvaru \"-n5\" anebo \"-n 5\".\n"
+                         "  Dlouhé parametry je nutné zadávat ve tvaru \"--num=5\".\n\n";
+
+void argParse(int argc, char * argv[], unsigned int * initInfectionRate);
 #endif //IMS_MAIN_H
