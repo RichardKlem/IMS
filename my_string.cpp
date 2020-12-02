74de53ff62448bc4534d72c050b16b968c653891
Richard Klem <rklem@codasip.com>
2020-12-02 22:33:17 +0100

    Add own string processing library

---
+++ my_string.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Richard Klem on 24.04.20.
+//
+#include <cstring>
+#include <sstream>
+#include "my_string.h"
+
+/**
+ * Převede řetězec na číslo
+ * @param str vstupní řetězec
+ * @return int výstupní struktura obsahující dvojici (status, číslo)
+ */
+str2int_struct_t str2int (char * str){
+    str2int_struct_t result {S2I_FAIL, 0};
+    unsigned int num;
+
+    if (str == nullptr)
+        return result;
+
+    // ošetření, že se nevyskytuje žádný NEnumerický znak
+    for (unsigned int i = 0; i < strlen(str); ++i) {
+        if (!isdigit(str[i]))
+            return result;
+    }
+
+    long int tmp = strtol(str, nullptr, 0);
+    num = (unsigned int) tmp;
+
+    result = {S2I_OK, num};
+    return result;
+}
---
+++ my_string.h
@@ -0,0 +1,14 @@
+/**
+ * @author: Richard Klem
+ * @email: xklemr00@stud.fit.vutbr.cz
+ * @login: xklemr00
+ */
+#ifndef PROJ2_MY_STRING_H
+#define PROJ2_MY_STRING_H
+enum STR2INT_STATUS_CODE {S2I_FAIL, S2I_OK};
+struct str2int_struct_t{
+    STR2INT_STATUS_CODE status;
+    unsigned int num;
+};
+str2int_struct_t str2int(char * str);
+#endif //PROJ2_MY_STRING_H
