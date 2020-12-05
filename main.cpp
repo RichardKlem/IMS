0228d43e720ad5bb0f0c64013fbd9e26e84d4a40
Richard Klem <rklem@codasip.com>
2020-12-05 19:24:23 +0100

    Add new arguments to argparse() call

--- main.cpp
+++ main.cpp
@@ -22,7 +22,8 @@ int main(int argc, char *argv[]) {
                                                     {17, 14}, {17, 15}, {17, 16}, {17, 17},
                                                     {17, 18}};
     if (argc > 1)
-        argParse(argc, argv, &number, &initInfectionRate, &initImmuneRate, &x, &y, &time, &step);
+        argParse(argc, argv, &number, &initInfectionRate, &initImmuneRate, &x, &y, &time, &step, &forwardP, &rightP,
+                 &leftP, &backP, &stayP);
 
     CellularAutomaton CA(x, y, number, &walls);
     CA.initWalls(&CA.getMatrix());
