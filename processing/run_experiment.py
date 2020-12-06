9bb1a9208fabd31b3e8948a341066473deef66c8
Richard Klem <rklem@codasip.com>
2020-12-06 03:59:50 +0100

    Script to generate average cycles and report table.

---
+++ processing/run_experiment.py
@@ -0,0 +1,78 @@
+import argparse
+import logging
+import numpy as np
+import os
+import pandas as pd
+import pickle
+import re
+import shutil
+import stat
+import sys
+
+
+RESULT_FILE = "results{0}.txt"
+HEADER = ["#", "N(0)", "S(0)", "I(0)", "p_f", "p_b", "p_r", "p_l", "p_s", "Cycles"]
+BIN_NAME = "covid"
+
+PROJECT_DIRECTORY = os.path.dirname(os.getcwd())
+PYTHON_SCRIPT_DIR = os.getcwd()
+
+BUILD_CMD = f"cmake -B  {PYTHON_SCRIPT_DIR}/build/ -S {PROJECT_DIRECTORY} && cd ./build/ && make"
+MAKE_CMD = "make"
+RUN_CMD = "./" + BIN_NAME + " {0}"
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Experiments')
+    parser.add_argument("-p", "--parameters", action="store", default=os.path.join(os.getcwd(), "parameters.txt"),
+                        help="Select a path to a file with parameters.")
+    parser.add_argument("--work-dir", action="store", default=os.path.join(os.getcwd(), "build"),
+                        help="Path to a build directory.")
+    parser.add_argument("--result-dir", action="store", default=os.path.join(os.getcwd(), "results"),
+                        help="Path to a folder in which the results will be stored.")
+    logging.basicConfig(level=logging.INFO)
+
+    args = parser.parse_args(sys.argv[1:])
+
+    if os.path.isdir(args.work_dir):
+        shutil.rmtree(args.work_dir)
+    os.mkdir(args.work_dir)
+    os.chdir(PROJECT_DIRECTORY)
+    os.system(MAKE_CMD)
+    os.chdir(PYTHON_SCRIPT_DIR)
+    shutil.copyfile(os.path.join(PROJECT_DIRECTORY, BIN_NAME), os.path.join(args.work_dir, BIN_NAME))
+    os.chdir(args.work_dir)
+    with open(args.parameters, 'r') as parameters:
+        extracted_args = []
+        for experiment in parameters.readlines():
+            if experiment != os.linesep:
+                arguments = re.match(r"(\d{1,2})\.\s(.*)", experiment)
+                if os.path.isdir(os.path.join(args.work_dir, f"experiment{arguments.group(1)}")):
+                    shutil.rmtree(os.path.join(args.work_dir, f"experiment{arguments.group(1)}"))
+                os.mkdir(os.path.join(args.work_dir, f"experiment{arguments.group(1)}"))
+                print(f"experiment {arguments.group(1)}")
+                if arguments is not None:
+                    os.chmod(BIN_NAME, os.stat(BIN_NAME).st_mode | stat.S_IEXEC)
+                    average_cycles = []
+                    for i in range(100):
+                        os.system((RUN_CMD.format(arguments.group(2))) + f" -d experiment{arguments.group(1)}")
+                        with open(os.path.join(f"experiment{arguments.group(1)}", "results.txt")) as result_file:
+                            cycles = int(result_file.readline())
+                            average_cycles.append(cycles)
+
+                    split_args = arguments.group(2).split(' ')
+                    extracted_args.append([arguments.group(1)] + split_args[1:-1:2] + [split_args[-1]] +
+                                          [str(sum(average_cycles) / len(average_cycles))])
+
+        with open("agregated_results", "ab+") as f:
+            pickle.dump(extracted_args, f)
+
+    with open("agregated_results", "rb") as f:
+        df = pd.DataFrame(np.array(pickle.load(f)).transpose(), HEADER).transpose()
+        with open("agregated_results.html", "w") as html_file:
+            html_file.writelines(df.to_html())
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
